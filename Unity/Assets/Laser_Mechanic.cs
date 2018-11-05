a5a55a4b0616acb265b042215330ed6092c6c5b0
Fred-Wright <fredwright2000@live.co.uk>
2018-11-05 11:40:43 +0000

    initial commit of project

---
+++ Unity/Assets/Laser_Mechanic.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser_Mechanic : MonoBehaviour {
+
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    public float laserSpawnRate = 0.25f;
+    
+
+
+    void Start()
+    {
+        InvokeRepeating("Spawn", laserSpawnRate, laserSpawnRate);
+    }
+
+    void Spawn()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+
+
+
+
+
+
+    // Update is called once per frame
+    /*   void Update () {
+           if (Input.GetButtonDown("Fire1"))
+           {
+               Shoot();
+           }
+       }
+       // this should be the shootey bit
+       void Shoot ()
+       {
+           Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+       }
+   */
+}
