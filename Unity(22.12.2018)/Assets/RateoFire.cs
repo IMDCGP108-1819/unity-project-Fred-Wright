efadfe6f17bcdfce920319177a0f5bf0848eb2f2
Fred-Wright <fredwright2000@live.co.uk>
2018-12-23 01:06:43 +0000

    Looks Upgrades & Stuff

---
+++ Unity(22.12.2018)/Assets/RateoFire.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RateoFire : MonoBehaviour
+{
+
+    public void RateOfFire(string level)
+    {
+
+        if (PlayerPrefs.GetInt("Credits") > PlayerPrefs.GetInt("RoFCost"))
+        {
+            PlayerPrefs.SetInt("Credits", PlayerPrefs.GetInt("Credits") - PlayerPrefs.GetInt("RoFCost"));
+            PlayerPrefs.SetInt("RoFCost", PlayerPrefs.GetInt("RoFCost") + (PlayerPrefs.GetInt("RoFCost")));  //Timesing didnt work...
+            PlayerPrefs.SetInt("RateOfFire", PlayerPrefs.GetInt("RateOfFire") + 1);
+        }
+
+    }
+
+}
